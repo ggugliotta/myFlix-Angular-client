77ac939e95f8972fd512afed9f1aed76a8fde396
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2024-01-03 20:30:43 -0600

    changes to dialogs

--- src/app/genre/genre.component.ts
+++ src/app/genre/genre.component.ts
@@ -8,6 +8,10 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrl: './genre.component.css'
 })
 export class GenreComponent {
+
+  movies: any[] = [];
+  user: any = {};
+
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
