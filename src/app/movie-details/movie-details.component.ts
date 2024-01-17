f5a9b73853a35b58e2738b8e409a3ef46e230780
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2024-01-17 11:51:16 -0600

    added interpolation for movie details dialog

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -9,7 +9,7 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialo
 export class MovieDetailsComponent {
   constructor(
     @Inject(MAT_DIALOG_DATA)
-    public data: {
+    public movies: {
       title: string; 
       imageUrl: string;
       description: string;
