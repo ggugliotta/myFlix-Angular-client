27e657e591c1486bc3f7752fe6d4833c8ce0a5ba
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2024-01-17 13:17:41 -0600

    added navbar html styling

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router,
+    public snackBar: MatSnackBar
+  ) { }
+  /**
+   * logs out user and clears local storage
+   */
+  logoutUser(): void {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    console.log('logout successful');
+    this.snackBar.open('Logout successful', 'OK', {
+      duration: 2000
+    });
+    this.router.navigate(['welcome']);
+  }
+
+  /**
+   * @returns a boolean if user is currently logged in by checking local storage
+  */
+ isUserLoggedIn(): boolean {
+  const user = localStorage.getItem('user');
+  return user !== null && user !== undefined; 
+ }
 
 }
